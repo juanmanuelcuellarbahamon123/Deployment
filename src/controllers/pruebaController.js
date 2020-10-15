496609edc6aa4ad6b2dd5b2d3fd7076bd090b537
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-14 22:52:59 -0500

    test database
    test database

--- src/controllers/pruebaController.js
+++ src/controllers/pruebaController.js
@@ -1,3 +1,12 @@
+const mysql = require('../database');
+
 exports.prueba = (req,res) => {
-    res.render('home/index');
+
+    const consulta = mysql.query("SELECT * FROM personas");
+
+    let data = {
+        personas: consulta
+    }
+
+    res.render('home/index', data);
 }
