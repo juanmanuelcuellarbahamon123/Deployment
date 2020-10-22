d4798f56a9ec60806eb4d128b62262610c4fec3c
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-22 07:33:05 -0500

    ssss
    ssss

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const usuarioController = require('../controllers/usuarioController');
 
 router.get('/usuario', usuarioController.vistaUsuario);
-router.get('/menu', usuarioController.menu);
-router.get('/pedido', usuarioController.pedido);
+//router.get('/menu', usuarioController.menu);
+//router.get('/pedido', usuarioController.pedido);
 
 module.exports = router;
