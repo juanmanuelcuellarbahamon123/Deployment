185b725695ccf3d2830a05b67dcb9c537ba21d75
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-21 20:39:46 -0500

    fsed
    ewf

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -21,8 +21,7 @@ exports.registrarUsuario = async (req,res) => {
         if(err) {
             res.status(401).json({ err: err });
         } else {
-            res.redirect('/');
-            console.log(resultado);
+            res.status(200).json({ msg: resultado });
         }
     });
 }
