83363cca3dccff4eef24adebea31f769617f3b32
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-21 23:34:38 -0500

    gay
    gay

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -3,3 +3,11 @@ const mysql = require('../database');
 exports.vistaAdmin = (req,res) => {
     res.render('admin/index');
 }
+
+exports.usuarios = (req,res) => {
+    res.render('admin/usuarios');
+}
+
+exports.productos = (req,res) => {
+    res.render('admin/productos');
+}
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,5 +2,7 @@ const router = require('express').Router()
 const adminController = require('../controllers/adminController');
 
 router.get('/admin', adminController.vistaAdmin);
+router.get('/usuarios', adminController.usuarios);
+router.get('/productos', adminController.productos);
 
 module.exports = router;
