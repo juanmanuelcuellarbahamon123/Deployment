f65d647b0b1a0a5e22b3baf97a98a6a99cf109ab
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-21 19:13:43 -0500

    partial
    partial

--- src/controllers/pruebaController.js
+++ src/controllers/pruebaController.js
@@ -1,21 +1,9 @@
 const mysql = require('../database');
 
 exports.prueba = async (req,res) => {
-
-    /*
-    const consulta = await mysql.query("SELECT * FROM personas");
-
-    console.log(consulta);
-
-    let data = {
-        personas: consulta
-    }
-    */
-
     res.render('home/index');
 }
 exports.iniciar = (req,res)=>{
-
     console.log('hi');
     res.status(200).json({ msg: 'Ya funciono la api' });
 }
--- src/routes/prueba.js
+++ src/routes/prueba.js
@@ -2,6 +2,5 @@ const router = require('express').Router()
 const pruebaController = require('../controllers/pruebaController');
 
 router.get('/', pruebaController.prueba);
-router.get('/login',pruebaController.iniciar);
 
 module.exports = router;
