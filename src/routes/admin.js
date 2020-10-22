ea47dc6ce140977dc30c0c2bf6840de61b3eba4b
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-21 21:38:55 -0500

    ewf
    wef

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const adminController = require('../controllers/adminController');
 
-router.get('/admin', usuarioController.vistaAdmin);
+router.get('/admin', adminController.vistaAdmin);
 
 module.exports = router;
