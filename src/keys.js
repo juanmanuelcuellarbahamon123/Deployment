bd6b9e9f272568db754f9a2ad90ab2feb06c9a4c
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-21 20:54:30 -0500

    fwef
    wef

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'holis'
+        host: 'b8km0ddzgb2pd0z63fqy-mysql.services.clever-cloud.com',
+        user: 'um5yu6kd025jdrqi',
+        password: 'S73rH8sMNYs1fZWEJ0PH',
+        database: 'b8km0ddzgb2pd0z63fqy'
     }
 }
