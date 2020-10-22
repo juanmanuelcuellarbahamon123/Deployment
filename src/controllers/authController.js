9c68e7d6cffb8bc1f1357a1c5cf4c8b361d65be8
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-21 19:43:21 -0500

    wed
    wdw

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,10 @@ exports.index = (req,res) => {
     res.render('auth/login');
 }
 
+exports.formRegistro = (req,res) => {
+    res.render('auth/registro');
+}
+
 exports.registrarUsuario = async (req,res) => {
     await mysql.query("INSERT INTO usuarios(nombre, apellido, correo, password) SET = ?", req.body);
 }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 const authController = require('../controllers/authController');
 
 router.get('/', authController.index);
+router.get('/registro', authController.formRegistro);
+
 router.post('/registro', authController.registrarUsuario);
 
 module.exports = router;
