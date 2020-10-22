6138a887e876b31f9fe581dd06008b5ca3740f51
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-21 20:10:05 -0500

    tgerger
    aergarwe

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -16,9 +16,9 @@ exports.registrarUsuario = async (req,res) => {
         password: req.body.password,
         id_rol: 2
     }
-    await mysql.query("INSERT INTO usuarios SET ?", [newUser])
-        .catch(err => {
-            console.log(error);
-        });
+    await mysql.query("INSERT INTO usuarios SET ?", newUser, (err, resultado) => {
+        if(err) console.error(err);
+        console.log(resultado);
+    });
     res.redirect('/');
 }
